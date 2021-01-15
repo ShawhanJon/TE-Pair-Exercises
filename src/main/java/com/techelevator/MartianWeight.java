483e1ce01b8b2a1c85738e575d7ccea0891327b2
Student <student@Techs-Air.attlocal.net>
2021-01-15 16:10:06 -0500

    Martian Weight complete

--- src/main/java/com/techelevator/MartianWeight.java
+++ src/main/java/com/techelevator/MartianWeight.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  In case you've ever pondered how much you weight on Mars, here's the calculation:
  	Wm = We * 0.378
@@ -19,6 +21,24 @@ Enter a series of Earth weights (space-separated): 98 235 185
 public class MartianWeight {
 
 	public static void main(String[] args) {
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		//prompt for earth weights
+		System.out.println("Please enter 3 Earth weights separated by spaces: ");
+		String answer = myScanner.nextLine();
+		
+		//split earth weights into an array
+		String[] earthweights = answer.split(" ");
+		
+		for (int i = 0; i < earthweights.length; i++) {
+			
+			int earthfloat = Integer.parseInt(earthweights[i]);
+			
+			int marsweight = (int) (earthfloat * 0.378);
+			
+			System.out.println(earthweights[i] + "lbs. on Earth, is " + marsweight + "lbs. on Mars.");
+		}
 
 	}
 
