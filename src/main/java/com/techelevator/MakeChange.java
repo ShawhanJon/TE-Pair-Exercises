de63ece13e5c6480e8bfd8ca20120792a8a283df
Student <student@Techs-MacBook-Air.local>
2021-01-15 15:25:10 -0500

    Change Exercise Complete

--- src/main/java/com/techelevator/MakeChange.java
+++ src/main/java/com/techelevator/MakeChange.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  Write a command line program which prompts the user for the total bill, and the amount tendered. It should then
  display the change required.
@@ -12,6 +14,26 @@ package com.techelevator;
 public class MakeChange {
 
 	public static void main(String[] args) {
+		
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("Please enter the amount of the bill:\n");
+		String billed = myScanner.nextLine();
+		
+		
+		double billAmount = Double.parseDouble(billed);
+		
+		System.out.println("Please enter the amount tendered:\n");
+		String tendered = myScanner.nextLine();
+		
+		double tenderAmount = Double.parseDouble(tendered);
+		
+		if (tenderAmount < billAmount) {
+			System.out.println("$" + (billAmount - tenderAmount) + " Needed");
+		}
+		else {	
+		System.out.println("The change required is " + "$" + (tenderAmount - billAmount));
+		}
 
 	}
 
